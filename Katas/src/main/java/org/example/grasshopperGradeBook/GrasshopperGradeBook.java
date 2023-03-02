c1525ebde75b6f7cefc5c8dfb9e1350490a4912c
derFabio <fabio.virgolini@gmail.com>
2023-03-02 17:45:01 +0100

    Grasshopper Grade Book kata

---
+++ Katas/src/main/java/org/example/grasshopperGradeBook/GrasshopperGradeBook.java
@@ -0,0 +1,18 @@
+package org.example.grasshopperGradeBook;
+
+public class GrasshopperGradeBook {
+    public char getGrade(int s1, int s2, int s3) {
+        float score = (s1+s2+s3) / 3;
+        if (score >= 90) {
+            return 'A';
+        } else if (score >= 80) {
+            return 'B';
+        } else if (score >= 70) {
+            return 'C';
+        } else if (score >= 60) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
+}
