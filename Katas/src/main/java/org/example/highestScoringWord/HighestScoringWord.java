2420ed4bcfd5603a2d06ee5a91769cf3fd045fea
derFabio <fabio.virgolini@gmail.com>
2023-03-06 00:02:15 +0100

    Highest Scoring Word with Streams works now

--- Katas/src/main/java/org/example/highestScoringWord/HighestScoringWord.java
+++ Katas/src/main/java/org/example/highestScoringWord/HighestScoringWord.java
@@ -2,17 +2,17 @@ package org.example.highestScoringWord;
 
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
 public class HighestScoringWord {
     public String high(String s) {
         return Arrays.stream(s.split("\\s+"))
-                .max(Comparator.comparingInt(HighestScoringWord::calculateScore)
-                .thenComparingInt(s::indexOf))
+                .max(Comparator.comparingInt(HighestScoringWord::calculateScore))
                 .orElse("");
     }
 
     public static int calculateScore(String word) {
-        return word.chars().map(c -> c - 'a' + 1).sum();
+        return word.chars()
+            .map(c -> c - 'a' + 1)
+            .sum();
     }
 }
