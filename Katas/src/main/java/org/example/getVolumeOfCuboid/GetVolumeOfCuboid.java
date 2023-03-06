d357ad805a4d42b2d8de9a25b13e0774e9bcda9e
derFabio <fabio.virgolini@gmail.com>
2023-03-06 01:16:41 +0100

    GetVolumeOfACuboid Kata done

---
+++ Katas/src/main/java/org/example/getVolumeOfCuboid/GetVolumeOfCuboid.java
@@ -0,0 +1,7 @@
+package org.example.getVolumeOfCuboid;
+
+public class GetVolumeOfCuboid {
+    public double getVolumeOfCuboid(final double length, final double width, final double height) {
+        return length*width*height;
+    }
+}
--- Katas/src/main/java/org/example/lostWithoutAMap/LostWithoutAMap.java
+++ Katas/src/main/java/org/example/lostWithoutAMap/LostWithoutAMap.java
@@ -3,7 +3,7 @@ package org.example.lostWithoutAMap;
 import java.util.Arrays;
 
 public class LostWithoutAMap {
-    public static int[] map(int[] arr) {
+    public int[] map(int[] arr) {
         return Arrays.stream(arr).map(i->i*2).toArray();
     }
 }
