6e88a8692018314971e7c0d2d1756c406eebebab
derFabio <fabio.virgolini@gmail.com>
2023-03-18 21:06:40 +0100

    Rental Car Cost Kata added

---
+++ Katas/src/main/java/org/example/rentalCarCost/RentalCarCost.java
@@ -0,0 +1,7 @@
+package org.example.rentalCarCost;
+
+public class RentalCarCost {
+    public int rentalCarCost(int d) {
+        return (d < 3) ? (d*40) : (d < 7) ? (d*40-20) : (d*40-50);
+    }
+}
