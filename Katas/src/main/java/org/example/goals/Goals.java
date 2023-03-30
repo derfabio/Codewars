f9ce296e6095dd95a832bd716716f4bcc12a5059
derFabio <fabio.virgolini@gmail.com>
2023-03-30 14:55:35 +0200

    Goals Kata

---
+++ Katas/src/main/java/org/example/goals/Goals.java
@@ -0,0 +1,8 @@
+package org.example.goals;
+
+public class Goals {
+        public static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+            return laLigaGoals+copaDelReyGoals+championsLeagueGoals;
+        }
+    }
+}
