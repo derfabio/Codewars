09f7be8f76e8a003547b4b55cde5de854cd6daca
derFabio <fabio.virgolini@gmail.com>
2023-04-07 13:25:24 +0200

    Between Kata

---
+++ Katas/src/main/java/between/Between.java
@@ -0,0 +1,10 @@
+package between;
+
+import java.util.stream.IntStream;
+
+public class Between {
+
+    public static int[] between(int a, int b) {
+        return IntStream.rangeClosed(a, b).toArray();
+    }
+}
