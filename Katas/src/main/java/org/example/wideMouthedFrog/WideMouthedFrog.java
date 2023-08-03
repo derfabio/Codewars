3f17b137365aacfae5b46e93e82527a202ff864a
derFabio <fabio.virgolini@gmail.com>
2023-08-03 14:01:27 +0200

    WideMouthedFrog kata added

---
+++ Katas/src/main/java/org/example/wideMouthedFrog/WideMouthedFrog.java
@@ -0,0 +1,11 @@
+package main.java.org.example.wideMouthedFrog;
+
+public class WideMouthedFrog {
+    public static String mouthSize(String animal){
+        if (animal.equalsIgnoreCase("alligator")) {
+            return "small";
+        } else {
+            return "wide";
+        }
+    }
+}
