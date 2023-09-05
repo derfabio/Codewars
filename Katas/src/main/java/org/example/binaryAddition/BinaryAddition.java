99e294dfe8a15e202f5f61c102f2442041c6db6a
derFabio <fabio.virgolini@gmail.com>
2023-09-05 16:17:44 +0200

    BinaryAddition kata added

---
+++ Katas/src/main/java/org/example/binaryAddition/BinaryAddition.java
@@ -0,0 +1,7 @@
+package main.java.org.example.binaryAddition;
+
+public class BinaryAddition {
+    public static String binaryAddition(int a, int b){
+        return Integer.toBinaryString(a+b);
+    }
+}
